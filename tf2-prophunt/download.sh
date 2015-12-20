0ea7d16d1405dc781032bb78effec44d04782dcd
Chris H <squid@sirsquidness.com>
2015-12-20 20:58:57 +1100

    Add TF2 and TF2 Prophunt

---
+++ tf2-prophunt/download.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+MMVERSION=$( curl http://www.metamodsource.net/downloads/ | grep -o "mmsource-[0-9\.]*-linux.tar.gz" | head -n 1 )
+curl -o $MMVERSION http://www.gsptalk.com/mirror/sourcemod/$MMVERSION
+
+SMVERSION=$(  curl http://www.sourcemod.net/downloads.php?branch=stable | grep -Eo "sourcemod-.*?-linux.tar.gz" | head -n 1 )
+
+SMMAJORVERSION=$( echo $SMVERSION | grep -Eo "\-[0-9]*\.[0-9]*" |  grep -Eo "[0-9]*\.[0-9]*")
+
+echo $SMVERSION
+echo $SMMAJORVERSION
+
+SMURL="http://www.sourcemod.net/smdrop/$SMMAJORVERSION/$SMVERSION"
+curl -o $SMVERSION $SMURL
+
+
+TF2ITEMSURL=$(   curl "https://builds.limetech.org/?p=tf2items" | grep -Eo "tf2items-.*?-linux.zip" | head -n 1)
+curl -o $TF2ITEMSURL https://builds.limetech.org/files/$TF2ITEMSURL
---
+++ tf2/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./srcds_run -game tf +sv_lan 1 -ip 0.0.0.0 +hostname tessst +map cp_hydro -maxplayers $MAXPLAYERS
+
