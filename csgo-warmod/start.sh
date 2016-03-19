63600b28ff4dbebbdd5f387cf4e1b5f74295d90e
Chris H <squid@sirsquidness.com>
2016-03-19 21:52:42 +1100

    Add start script stuff for csgo

--- csgo-warmod/start.sh
+++ csgo-warmod/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-./srcds_run -game csgo +sv_lan 1 -ip 0.0.0.0 +hostname tessst +map cs_office -maxplayers $MAXPLAYERS
+
+echo "hostname 'eLeague CS:GO 5 v 5 Match Server #$SERVER_NUM'" > csgo/cfg/hostname.cfg 
+echo '"STEAM_0:0:19457778" "@Full Admins"' > csgo/addons/sourcemod/configs/admins_simple.ini
+
+./srcds_run -game csgo +sv_lan 1 -ip 0.0.0.0 +map cs_office -maxplayers $MAXPLAYERS -usercon -exec hostname.cfg -hostname "eLeague CS:GO 5 v 5 Match Server #$SERVER_NUM" +game_type 0 +game_mode 1
 
