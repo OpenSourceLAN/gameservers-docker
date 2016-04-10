977dc6e2887562783aceaa9f12923650cc53112d
Chris Holman <squid@sirsquidness.com>
2016-04-10 22:22:23 +1000

    Remove broken reference to a cfg folder, improve start script

--- csgo/start.sh
+++ csgo/start.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
+if [ -z $SERVER_TOKEN ]
+then
+   echo "No server token supplied" >&2
+   exit 1;
+fi
 
-echo "hostname 'eLeague CS:GO 5 v 5 Match Server #$SERVER_NUM'" > csgo/cfg/hostname.cfg 
-echo '"STEAM_0:0:19457778" "@Full Admins"' > csgo/addons/sourcemod/configs/admins_simple.ini
+[ $LAN -ge 1 ] && LAN="1" || LAN="0"
 
-./srcds_run -game csgo +sv_lan 1 -ip 0.0.0.0 +map cs_office -maxplayers 12 -usercon -exec hostname.cfg -hostname "eLeague CS:GO 5 v 5 Match Server #$SERVER_NUM" +game_type 0 +game_mode 1 -tickrate 128 -maxplayers_override 12
+[ -n $HOSTNAME ] || HOSTNAME="CS:GO Server"
+[ -n $GAME_TYPE ] || GAME_TYPE=0
+[ -n $GAME_MODE ] || GAME_MODE=1
+[ -n $MAP ] || MAP="cs_office"
+[ -n $OTHER_ARGS ] || OTHER_ARGS=""
+
+if [ -n $SOURCETV_ADDR ]
+then
+   echo "tv_title $HOSTNAME" > csgo/cfg/hostname.cfg 
+   ./srcds_run -game csgo -console -tickrate 128 -maxplayers 64 +tv_relay $SOURCETV_ADDRESS $OTHER_ARGS
+
+else
+
+   echo "hostname $HOSTNAME" > csgo/cfg/hostname.cfg 
+
+   ./srcds_run -game csgo +sv_lan $LAN -ip 0.0.0.0 +map $MAP -maxplayers 12 -usercon -exec hostname.cfg +game_type 0 +game_mode 1 -tickrate 128 -maxplayers_override 12 $OTHER_ARGS 
+
+
+fi
+
+
+#echo '"STEAM_0:0:19457778" "@Full Admins"' > csgo/addons/sourcemod/configs/admins_simple.ini
+
 
