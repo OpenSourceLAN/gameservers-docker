dd726c53e2562ded87d97c8985376937661a0173
Chris Holman <squid@sirsquidness.com>
2016-10-31 20:25:26 +1100

    Add csgo 5v5 config

---
+++ csgo-comp/start-comp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# classic competitive
+export GAME_TYPE=0
+export GAME_MODE=1
+
+export LAN=1
+export MAX_PLAYERS=11
+export MAP="de_dust2"
+export OTHER_ARGS="+mapgroup mg_active"
+
+exec ./start.sh $@
