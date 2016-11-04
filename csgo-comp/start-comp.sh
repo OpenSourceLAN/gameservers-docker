41f6ba2a11a5f857fe1b570cd9d041040f2dda00
Chris H <squid@sirsquidness.com>
2016-11-04 23:05:26 +1100

    Making changes during event

--- csgo-comp/start-comp.sh
+++ csgo-comp/start-comp.sh
@@ -7,6 +7,6 @@ export GAME_MODE=1
 export LAN=1
 export MAX_PLAYERS=11
 export MAP="de_dust2"
-export OTHER_ARGS="+mapgroup mg_active"
+export OTHER_ARGS="$OTHER_ARGS +mapgroup mg_active"
 
 exec ./start.sh $@
--- csgo-gg/start-gg.sh
+++ csgo-gg/start-gg.sh
@@ -2,7 +2,7 @@
 
 export GAME_TYPE=1
 export GAME_MODE=0
-export OTHER_ARGS="+mapgroup mg_armsrace"
+export OTHER_ARGS="+mapgroup mg_armsrace -tickrate 128"
 export MAP="ar_shoots"
 export MAXPLAYERS=32
 exec ./start.sh 
