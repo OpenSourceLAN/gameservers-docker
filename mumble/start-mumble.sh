7fbdc377072a4235a3c8b6fd65f3bf8111440df7
Chris Holman <squid@sirsquidness.com>
2017-02-05 12:06:09 +1100

    Add some useful config options to mumble

--- mumble/start-mumble.sh
+++ mumble/start-mumble.sh
@@ -9,9 +9,9 @@ mkdir -p /mumble/data
 
 cp mumble.ini $RUN_CONFIG
 
-[[ -n $MUMBLE_PASSWORD ]] && echo "password=$MUMBLE_PASSWORD" >> $RUN_CONFIG
-[[ -n $MUMBLE_USERS ]] && echo "users=$MUMBLE_USERS" >> $RUN_CONFIG
-[[ -n $MUMBLE_USERSPERCHANNEL ]] && echo "usersperchannel=$MUMBLE_USERSPERCHANNEL" >> $RUN_CONFIG
-[[ -n $MUMBLE_WELCOMETEXT ]] && echo "welcometext=$MUMBLE_WELCOMETEXT" >> $RUN_CONFIG
+[[ -n "$MUMBLE_PASSWORD" ]] && echo "password=$MUMBLE_PASSWORD" >> $RUN_CONFIG
+[[ -n "$MUMBLE_USERS" ]] && echo "users=$MUMBLE_USERS" >> $RUN_CONFIG
+[[ -n "$MUMBLE_USERSPERCHANNEL" ]] && echo "usersperchannel=$MUMBLE_USERSPERCHANNEL" >> $RUN_CONFIG
+[[ -n "$MUMBLE_WELCOMETEXT" ]] && echo "welcometext=$MUMBLE_WELCOMETEXT" >> $RUN_CONFIG
 
 exec ./murmur.x86 -fg -ini $RUN_CONFIG
