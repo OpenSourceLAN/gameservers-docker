74ed4c4b8d2743d9e7e68306ea9e05cb3101969b
Chris Holman <squid@sirsquidness.com>
2017-02-19 22:10:39 +1100

    Fix bsh typo and make wine compile to correct directory

--- common.sh
+++ common.sh
@@ -12,7 +12,6 @@ docker_build() {
 	[[ -z $NAME ]] && echo "No name provided for docker build" && exit 1
 
 	local PARENT="$(get_docker_parent $NAME)"
-	echo $PARENT
 	[[ -n $PARENT ]] && docker_build_if_needed $PARENT
 
 	(cd $BASE_DIR/$NAME && docker build -t $NAME -t $NAME:$(gettimestamp) . )
@@ -21,7 +20,7 @@ docker_build() {
 # Builds a container if it doesn't already have a latest
 docker_build_if_needed() {
 	local NAME=$1
-	( [[ "$(docker images --quiet $NAME:latest)" == "" ]] && docker_build $NAME
+	[[ "$(docker images --quiet $NAME:latest)" == "" ]] && docker_build $NAME
 }
 
 get_docker_parent() {
