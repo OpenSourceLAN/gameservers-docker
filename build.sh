aa3166af1900d1702a4fdb6679d603653b71d5f1
Chris Holman <squid@sirsquidness.com>
2017-04-19 21:46:26 +1000

    Change root build script to always use the folder specific build scripts

--- build.sh
+++ build.sh
@@ -3,6 +3,5 @@
 NAME=$1
 [[ -z $NAME ]] && echo "No image name given" && exit 1
 
-. ./common.sh
-
-docker_build $NAME
+[[ -f $NAME/build.sh ]] || (echo "Build file doesn't exist for $NAME/build.sh :( " && exit 1)
+[[ -f $NAME/build.sh ]] && (cd $NAME && ./build.sh )
