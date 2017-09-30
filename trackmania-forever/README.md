2f8d7aaf3caa4a1acc0313c395fef2048888e96e
Chris Holman <squid@sirsquidness.com>
2017-09-30 17:01:21 +1000

    Update trackmania-forever, add some extra tracklists

--- trackmania-forever/README.md
+++ trackmania-forever/README.md
@@ -1,4 +1,10 @@
+# Trackmania Nations Forever
 
-Useful links:
+
+### Useful links:
 * [server admin GUI](http://cyrlaur.free.fr/ServerMania/download.php)
 
+### Environment Variables
+
+* `SERVER_NAME` - Name server appears as in browser
+* `TRACKLIST` - file name of the tracklist from this directory to use (eg, `-e "TRACKLIST=tracklist-red.cfg"`)
--- trackmania-forever/start_server.sh
+++ trackmania-forever/start_server.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-exec ./TrackmaniaServer /lan /game_settings=tracklist.cfg /dedicated_cfg=dedicated_cfg.txt /nodaemon
+
+sed -i "s/%%SERVER_NAME%%/$SERVER_NAME/" /tm/GameData/Config/dedicated_cfg.txt
+
+[[ -n $TRACKLIST ]] || TRACKLIST="tracklist-all.cfg"
+
+exec ./TrackmaniaServer /lan /game_settings=$TRACKLIST /dedicated_cfg=dedicated_cfg.txt /nodaemon
 
