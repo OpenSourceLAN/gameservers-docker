ce409a03c2847a2ac1dbf906769a71ebe27da3da
Matthew Hayward <matthewh86@gmail.com>
2018-08-27 15:56:11 +0100

    Removed environment/config variables which did not work
    The ones specified required the left4downtown plugin

--- l4d2-metamod/start-l4d2.sh
+++ l4d2-metamod/start-l4d2.sh
@@ -4,14 +4,12 @@
 [[ $LAN -ge 1 ]] && LAN="1" || LAN="0"
 [[ -z $SERVER_IP ]] && SERVER_IP=0.0.0.0
 [[ -z "$MAP" ]] && MAP="c1m4_atrium"
-[[ -z $MAXPLAYERS ]] && MAXPLAYERS="8"
 [[ -z $MP_GAMEMODE ]] && MP_GAMEMODE="versus"
 
 # Config file
 [[ -n $RCON_PASSWORD ]] && RCON_PASSWORD="$RCON_PASSWORD"
 [[ -n $SV_PASSWORD ]] && SV_PASSWORD="$SV_PASSWORD"
 [[ -z $SV_HOSTNAME ]] && SV_HOSTNAME="Left 4 Dead 2 - MetaMod"
-[[ -z $VERSUS_TEAM_LIMIT ]] && VERSUS_TEAM_LIMIT="4"
 
 # mp config
 
@@ -20,12 +18,6 @@ cat <<EOF >/steam/l4d2/left4dead2/cfg/server.cfg
 hostname $SV_HOSTNAME
 rcon_password $RCON_PASSWORD
 sv_password $SV_PASSWORD
-l4d_maxplayers $MAXPLAYERS
-sm_cvar l4d_maxplayers $MAXPLAYERS
-l4d_survivor_limit $VERSUS_TEAM_LIMIT
-sm_cvar l4d_survivor_limit $VERSUS_TEAM_LIMIT
-l4d_infected_limit $VERSUS_TEAM_LIMIT
-sm_cvar l4d_infected_limit $VERSUS_TEAM_LIMIT
 EOF
 
 echo '$FULL_ADMINS' > left4dead2/addons/sourcemod/configs/admins_simple.ini
