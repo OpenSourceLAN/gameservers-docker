eab7324a8385693a7ce7535306de3c07f3e9c590
Matthew Hayward <matthewh86@gmail.com>
2018-08-27 18:57:51 +0100

    Added extra environment variables for configuration

--- trackmania-forever/start_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sed -i s/%%SERVER_NAME%%/$SERVER_NAME/ /home/tm/GameData/Config/dedicated_cfg.txt
-
-[[ -n $TRACKLIST ]] || TRACKLIST="tracklist-all.cfg"
-
-exec ./TrackmaniaServer /lan /game_settings=$TRACKLIST /dedicated_cfg=dedicated_cfg.txt /nodaemon
-
