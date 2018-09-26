2ac50ec945b81e4ffb8e6127897a91a1b30887d3
Chris Holman <squid@sirsquidness.com>
2018-09-26 23:44:47 +1000

    Accept trackmania-forever RCON password

--- trackmania-forever/start_server.sh
+++ trackmania-forever/start_server.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+
+[[ -n $RCON_PASSWORD ]] || RCON_PASSWORD=$(dd if=/dev/urandom bs=20 count=1 2>/dev/null | base64)
+
 sed -i "s/%%SERVER_NAME%%/$SERVER_NAME/" /tm/GameData/Config/dedicated_cfg.txt
+
+sed -i "s/SuperAdminPassword/$RCON_PASSWORD/" /tm/GameData/Config/dedicated_cfg.txt
+sed -i "s/AdminPassword/$RCON_PASSWORD/" /tm/GameData/Config/dedicated_cfg.txt
+sed -i "s/UserPassword/$RCON_PASSWORD/" /tm/GameData/Config/dedicated_cfg.txt
 
 [[ -n $TRACKLIST ]] || TRACKLIST="tracklist-all.cfg"
 
