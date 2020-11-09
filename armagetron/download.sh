326f231fb822e09bf99504587c88b18c6409f6a2
propstg <g@prop.st>
2020-11-09 18:31:37 -0500

    update for new file extension

--- armagetron/download.sh
+++ armagetron/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-ls armagetronad-*.src.tar.gz 2>/dev/null || (
-	download_URL=$(curl http://www.armagetronad.org/downloads.php | grep -o "https://launchpad.net/armagetronad/.*/+download/armagetronad-.*.src.tar.gz")
+ls armagetronad-*.tbz 2>/dev/null || (
+	download_URL=$(curl http://www.armagetronad.org/downloads.php | grep -o "https://launchpad.net/armagetronad/.*/+download/armagetronad-[^\"]*.tbz")
 
 	wget $download_URL
 )
